64914e67dc7068408c9dad991245718433d57029
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-06-04 00:56:38 +0900

    style: fix table height with antdTheme setting

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
-
+import { ConfigProvider } from 'antd';
+import { antdTheme } from './styles/antdTheme';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <ConfigProvider theme={antdTheme}>
+        <App />
+      </ConfigProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
