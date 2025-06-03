380788ec418ef34b7ca2925d19ede087c25b221c
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-06-03 22:22:42 +0900

    feat: add form validation schema

---
+++ src/components/RecordForm/validation.ts
@@ -0,0 +1,55 @@
+import * as yup from 'yup';
+import type { FieldConfig } from '../../types/field/type';
+import { DEFAULT_FIELDS } from '../../constants/fields';
+import { FieldProperty, FieldType } from '../../types/field/enum';
+
+export const createValidationSchema = (fields: FieldConfig[]) => {
+  const schema: Record<string, yup.Schema<any>> = {};
+
+  fields.forEach((field) => {
+    const fieldType = field[FieldProperty.TYPE];
+    const fieldName = field[FieldProperty.DATA_INDEX];
+    const fieldLabel = field[FieldProperty.TITLE];
+
+    switch (fieldType) {
+      case FieldType.TEXT:
+        schema[fieldName] = yup
+          .string()
+          .max(20, `글자수 20을 초과할 수 없습니다.`)
+          .when('$required', {
+            is: true,
+            then: (schema) => schema.required(`${fieldLabel}을(를) 입력해주세요`),
+          });
+        break;
+      case FieldType.TEXTAREA:
+        schema[fieldName] = yup
+          .string()
+          .max(50, `글자수 50을 초과할 수 없습니다.`)
+          .when('$required', {
+            is: true,
+            then: (schema) => schema.required(`${fieldLabel}을(를) 입력해주세요`),
+          });
+        break;
+      case FieldType.DATE:
+        schema[fieldName] = yup.date().when('$required', {
+          is: true,
+          then: (schema) => schema.required(`${fieldLabel}을(를) 선택해주세요`),
+        });
+        break;
+      case FieldType.CHECKBOX:
+        schema[fieldName] = yup.boolean().transform((value) => (value === null ? false : value));
+        break;
+      case FieldType.SELECT:
+        schema[fieldName] = yup.string().when('$required', {
+          is: true,
+          then: (schema) => schema.required(`${fieldLabel}을(를) 선택해주세요`),
+        });
+        break;
+    }
+  });
+
+  return yup.object(schema);
+};
+
+export const schema = createValidationSchema(DEFAULT_FIELDS);
+export type FormData = yup.InferType<typeof schema>;
