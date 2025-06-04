dda82c9114afc865c2a9ff52cb31b01b8463f661
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-06-04 18:24:22 +0900

    fix: fix when click save button not saving

--- src/page/RecordTable/RecordForm/FormContent/FormFooter/FormFooter.tsx
+++ src/page/RecordTable/RecordForm/FormContent/FormFooter/FormFooter.tsx
@@ -5,15 +5,22 @@ import { FormFooterContainer } from './styles';
 interface FormFooterProps {
   isSubmitDisabled: boolean;
   handleClose: () => void;
+  handleSubmit: () => void;
 }
 
-const FormFooter = ({ isSubmitDisabled, handleClose }: FormFooterProps) => {
+const FormFooter = ({ isSubmitDisabled, handleClose, handleSubmit }: FormFooterProps) => {
   return (
     <FormFooterContainer>
       <Button htmlType="button" onClick={handleClose}>
         취소
       </Button>
-      <Button form="recordForm" type="primary" htmlType="submit" disabled={isSubmitDisabled}>
+      <Button
+        form="recordForm"
+        type="primary"
+        htmlType="submit"
+        disabled={isSubmitDisabled}
+        onClick={handleSubmit}
+      >
         저장
       </Button>
     </FormFooterContainer>
