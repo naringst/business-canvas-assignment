7dec125c86677ac3e21bdd94737b61e4a8a5421f
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-06-04 18:39:30 +0900

    perf: optimize rendering RecordTableHeader component

--- src/page/RecordTable/RecordTableHeader/index.tsx
+++ src/page/RecordTable/RecordTableHeader/index.tsx
@@ -1,16 +1,26 @@
 import { PlusOutlined } from '@ant-design/icons';
 
+import { memo, useMemo } from 'react';
+
 import { TableHeaderAddButton, TableHeaderContainer, TableHeaderTitle } from './styles';
 
-const RecordTableHeader = ({ onClickAdd }: { onClickAdd: () => void }) => {
+interface RecordTableHeaderProps {
+  onClickAdd: () => void;
+}
+
+const RecordTableHeader = memo(({ onClickAdd }: RecordTableHeaderProps) => {
+  const addIcon = useMemo(() => <PlusOutlined />, []);
+
   return (
     <TableHeaderContainer>
       <TableHeaderTitle>회원 목록</TableHeaderTitle>
-      <TableHeaderAddButton type="primary" icon={<PlusOutlined />} onClick={onClickAdd}>
+      <TableHeaderAddButton type="primary" icon={addIcon} onClick={onClickAdd}>
         추가
       </TableHeaderAddButton>
     </TableHeaderContainer>
   );
-};
+});
+
+RecordTableHeader.displayName = 'RecordTableHeader';
 
 export default RecordTableHeader;
